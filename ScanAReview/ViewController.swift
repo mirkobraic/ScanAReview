685fd8ea26e0c3416640dfc33f512b41b73e895c
Mirko Braic <mirko.braic@gmail.com>
2021-01-22 00:28:21 +0100

    Implement collection view UI

--- ScanAReview/ViewController.swift
+++ ScanAReview/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.contentInset.top += 20
     }
-
-
+    
+    @IBAction func composeButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reviewCell", for: indexPath) as! ReviewCollectionViewCell
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+}
